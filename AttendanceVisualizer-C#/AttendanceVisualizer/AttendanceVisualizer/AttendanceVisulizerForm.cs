a88ea03386932e43256ba9fdfbec0d22ba33937e
Mohammedaljaafari2019 <124896261+Mohammedaljaafari2019@users.noreply.github.com>
2023-10-08 13:00:43 +0300

    v3

--- AttendanceVisualizer-C#/AttendanceVisualizer/AttendanceVisualizer/AttendanceVisulizerForm.cs
+++ AttendanceVisualizer-C#/AttendanceVisualizer/AttendanceVisualizer/AttendanceVisulizerForm.cs
@@ -15,11 +15,13 @@ namespace AttendanceVisualizer
     public partial class AttendanceVisulizerForm : Form  
     {
          
-        Employelist list = new Employelist( );
+       private Employelist list = new Employelist( );
+        private Reader reader = new Reader();
+        private Drawer drawer = new Drawer();
         Excel.Workbook xWorkBook = Globals.ThisAddIn.Application.ActiveWorkbook;
         public AttendanceVisulizerForm()
         {
-            Reader reader = new Reader( );
+           
             InitializeComponent();
             list = reader.Read(xWorkBook,1,list);
             SheetContents_ListBox.Items.Clear();
@@ -34,19 +36,12 @@ namespace AttendanceVisualizer
         //override
 
 
-        private void ReadSheet_Button_Click(object sender, EventArgs e)
-        {
-            {
-               
-                
-            }
-
-        }
+      
         //override
         
         private void DrawButton_Click(object sender, EventArgs e)
         {
-            Drawer drawer = new Drawer();
+            
             Graphics g = TimeLineDrawingPanel.CreateGraphics();
                 string item = SheetContents_ListBox.SelectedItem.ToString();
             drawer.Draw(list, g,  item);
